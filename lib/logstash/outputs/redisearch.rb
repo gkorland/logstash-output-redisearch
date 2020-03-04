fb5d35d2e6e7dadb39ae7c159069e97e891bd186
Ankita Unhalkar <ankita.unhalkar@hashedin.com>
2020-03-04 16:05:17 +0530

    Modified index creation

--- lib/logstash/outputs/redisearch.rb
+++ lib/logstash/outputs/redisearch.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
+require "logstash/outputs/template"
 require "logstash/outputs/base"
 require 'redisearch-rb'
-require 'redis'
 require 'json'
 
 # An redisearch output will store data into Redisearch.
@@ -9,26 +9,29 @@ class LogStash::Outputs::Redisearch < LogStash::Outputs::Base
 
   config_name "redisearch"
   default :codec, "json"
-  
+
   config :host, :validate => :string, :default => "127.0.0.1"
   config :port, :validate => :number, :default => 6379
-  config :index, :validate => :string, :default => 'test_index'
-  config :schema, :validate => :array, :default => ['message','TEXT']
-  
+  config :index, :validate => :string
+
   public
   def register
-    @redis_client = Redis.new(host: @host, port: @port)
-    @redis_client.flushdb
-    @redisearch_client = RediSearch.new(@index, @redis_client)
-    @redisearch_client.create_index(@schema)
+    @template_installed = false
+    @redisearch_client = nil
     @codec.on_event(&method(:send_to_redisearch))
-
   end # def register
 
   public
   def receive(event)
     begin
+      unless @template_installed
+        data =  event.to_hash
+        params = {"fields"=>data.keys,"host"=>@host,"port"=>@port}
+        @redisearch_client=Index.installTemplate(params)
+        @template_installed = true
+      end
       @codec.encode(event)
+     
     rescue StandardError => e
       @logger.warn("Error encoding event", :exception => e,
                    :event => event)
@@ -40,7 +43,7 @@ class LogStash::Outputs::Redisearch < LogStash::Outputs::Base
       doc = JSON.parse(payload)
       id = rand(2**0..2**63)
       status=@redisearch_client.add_doc(id,doc)
-      @logger.info("Inserted Successfully") if status == "OK"
+      @logger.info("Event inserted successfully") if status == "OK"
     rescue => e
       @logger.warn("Failed to send event to Redisearch", :event => event,
                    :exception => e,
