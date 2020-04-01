23d14f43c1ff0189fa6e3909a69cd1e846e8b5ae
Ankita Unhalkar <ankita.unhalkar@hashedin.com>
2020-04-01 16:15:21 +0530

    Modified single event insertion to batch insertion

--- lib/logstash/outputs/redisearch.rb
+++ lib/logstash/outputs/redisearch.rb
@@ -3,20 +3,29 @@
 require "logstash/outputs/template"
 require "logstash/outputs/base"
 require 'redisearch-rb'
+require 'securerandom'
+require "stud/buffer"
 require 'json'
 require 'time'
 # An redisearch output will store data into Redisearch.
 class LogStash::Outputs::Redisearch < LogStash::Outputs::Base
-
+  include Stud::Buffer
   config_name "redisearch"
   default :codec, "json"
 
   config :host, :validate => :string, :default => "127.0.0.1"
   config :port, :validate => :number, :default => 6379
   config :index, :validate => :string, :default => nil
+  config :reconnect_interval, :validate => :number, :default => 1
+  config :batch_events, :validate => :number, :default => 10
+  config :batch_timeout, :validate => :number, :default => 2
 
   public
   def register
+    buffer_initialize(
+      :max_items => @batch_events,
+      :max_interval => @batch_timeout,
+    )
     params = {"host"=>@host,"port"=>@port,"index"=>@index}
     @idx = Index.new(params)
     @redisearch_client = @idx.default_index()
@@ -26,22 +35,38 @@ class LogStash::Outputs::Redisearch < LogStash::Outputs::Base
   public
   def receive(event)
     begin
-      # event_data = event.to_hash
-      # @idx.checkfields(event_data.keys)
       @codec.encode(event)
     rescue StandardError => e
       @logger.warn("Error encoding event", :exception => e,
                    :event => event)
     end
   end # def event
+ 
+  def flush(events, close=false)
+    #buffer_flush should pass here the :final boolean value.
+    @redisearch_client.add_docs(events)
+    @logger.info("Buffer Inserted Successfully", :length => events.length)
+  end
+
+  # called from Stud::Buffer#buffer_flush when an error occurs
+  def on_flush_error(e)
+    @logger.warn("Failed to send backlog of events to Redissearch",
+      :exception => e,
+      :backtrace => e.backtrace
+    )
+  end
+
+  def close
+      buffer_flush(:final => true)
+  end
 
   def send_to_redisearch(event, payload)
     begin
-      doc = JSON.parse(payload)
-      doc['@timestamp'] = Time.parse(doc['@timestamp']).to_i
-      id = rand(2**0..2**63)
-      status=@redisearch_client.add_doc(id,doc)
-      @logger.info("Event inserted successfully") if status == "OK"
+      doc_data = JSON.parse(payload)
+      doc_id = SecureRandom.uuid
+      document = [doc_id,doc_data]
+      buffer_receive(document)
+
     rescue => e
       @logger.warn("Failed to send event to Redisearch", :event => event,
                    :exception => e,
